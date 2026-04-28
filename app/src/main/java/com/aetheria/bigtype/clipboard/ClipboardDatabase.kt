a5716a054b85d37d405e796362e6778c2124e0e3
tonygrischke-art <tonygrischke@gmail.com>
2026-04-28 01:01:29 +0000

    fix: remove duplicate IME service and duplicate database class

--- app/src/main/java/com/aetheria/bigtype/clipboard/ClipboardDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.aetheria.bigtype.clipboard
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [ClipEntity::class], version = 1, exportSchema = false)
-abstract class ClipboardDatabase : RoomDatabase() {
-    abstract fun clipDao(): ClipDao
-}
