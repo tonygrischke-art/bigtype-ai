8ac3a3ba26c8c5dcc3e88fa310366b10a16218f2
kira <kira@example.com>
2026-05-19 00:20:39 -0400

    fix: Fix HiltTestRunner implementation

--- app/src/androidTest/java/com/aetheria/bigtype/HiltTestRunner.kt
+++ app/src/androidTest/java/com/aetheria/bigtype/HiltTestRunner.kt
@@ -3,7 +3,7 @@ package com.aetheria.bigtype
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
-import dagger.hilt.android.testing.HiltTestApplication
+import com.aetheria.bigtype.HiltTestApplication
 
 class HiltTestRunner : AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
