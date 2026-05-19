71fb4c905fb9fd634779d174e87a01d8282723b7
kira <kira@example.com>
2026-05-19 00:16:43 -0400

    fix: Add missing Hilt dependencies for test runner

---
+++ app/src/androidTest/java/com/aetheria/bigtype/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.aetheria.bigtype
+
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
